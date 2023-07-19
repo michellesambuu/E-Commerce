0f66cfb2952db1c1355ebd9b2ca40c7058a6b967
michellesambuu <michelle.sambuu@gmail.com>
2023-07-19 07:32:07 -0500

    updated models

--- models/index.js
+++ models/index.js
@@ -1,29 +1,39 @@
-const { Model, DataTypes } = require('sequelize');
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
 
-const sequelize = require('../config/connection.js');
+// Categories have many Products
+// Category is the SOURCE model, Product is the TARGET
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
-class Category extends Model {}
+// Products belongsTo Category
+// Foreign Key of target (Category) is added onto the source (Product)
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
-Category.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    category_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
-  }
-);
+/*
+ Setting up many-to-many relationship with Product being associated to Tag
+ by adding the foreign key product_id to the ProductTag table.
+*/
+Product.belongsToMany(Tag, { through: ProductTag, foreignKey: 'product_id' }); 
 
-module.exports = Category;
+/* 
+ Setting up many-to-many relationship with Tag being associated to Product
+ by adding the foreign key tag_id to the ProductTag table.
+*/
+Tag.belongsToMany(Product, { through: ProductTag, foreignKey: 'tag_id' });
+// Tag.belongsToMany(Product, { through: ProductTag});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
+
+
